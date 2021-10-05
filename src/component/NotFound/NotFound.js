f408b4e328fc00ed4ec13705cfa730c8099d1d88
sharfuddinzishan <sharfuddin.ahamed91@gmail.com>
2021-10-05 23:21:16 +0600

    [Updated]

--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -5,7 +5,7 @@ import './NotFound.css'
 const NotFound = () => {
     return (
         <div className="container text-center p-5">
-            <h1 className="text-warning display-1">404</h1>
+            <h1 className="text-warning display-1"><i class='bx bx-flashing bx-bell-minus'></i> 404</h1>
             <p className="text-light fw-bold display-1">The Page You Are Looking, Not Found</p>
             <NavLink
                 to="/"
@@ -14,7 +14,9 @@ const NotFound = () => {
                     color: "red"
                 }}
             >
+                <i class='bx bx-lg bxs-right-arrow-circle bx-fade-right'></i>
                 <button className="btn btn-lg btn-link btn-primary text-light"> HOMEPAGE </button>
+                <i class='bx bx-lg bxs-left-arrow-circle bx-fade-left'></i>
             </NavLink>
         </div>
     );
